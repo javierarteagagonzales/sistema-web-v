c60f76dceb9c49d0a777dcc16999c5fb2d1fce40
Javier Alejandro Arteaga Gonzales <132113806+javierarteagagonzales@users.noreply.github.com>
2024-06-09 10:00:59 -0500

    Update serializer y add view

--- virca/views.py
+++ virca/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializer import AcabadoSerializer
+from .models import Acabado
 # Create your views here.
+
+class VircaView(viewsets.ModelViewSet):
+    serializer_class = AcabadoSerializer
+    queryset = Task.objects.all()
