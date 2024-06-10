f3ccd949cb30677b58e1b07cccb478ed3f5e76f1
Javier Alejandro Arteaga Gonzales <132113806+javierarteagagonzales@users.noreply.github.com>
2024-06-09 23:50:38 -0500

    conectando a db

--- vback/settings.py
+++ vback/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,15 +80,19 @@ WSGI_APPLICATION = "vback.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'vircatex',
+#         'USER': 'postgres',
+#         'PASSWORD': 'prueba',
+#         'HOST': 'localhost',  # O la IP del servidor de PostgreSQL
+#         'PORT': '5432',  # El puerto por defecto de PostgreSQL
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'vircatex',
-        'USER': 'postgres',
-        'PASSWORD': 'prueba',
-        'HOST': 'localhost',  # O la IP del servidor de PostgreSQL
-        'PORT': '5432',  # El puerto por defecto de PostgreSQL
-    }
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
