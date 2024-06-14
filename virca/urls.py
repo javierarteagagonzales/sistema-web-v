9818079bb353375c834855b8b95a87833f3d1578
Javier Alejandro Arteaga Gonzales <132113806+javierarteagagonzales@users.noreply.github.com>
2024-06-14 02:08:27 -0500

    s

--- virca/urls.py
+++ virca/urls.py
@@ -4,9 +4,12 @@ from django.urls import path, include
 from .views import AcabadoViewSet
 from rest_framework.routers import DefaultRouter
 
+from . import views
+
 router = DefaultRouter()
 router.register(r'acabado', AcabadoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api/acabados/', views.listar_acabados, name='listar_acabados'),
 ]
--- virca/views.py
+++ virca/views.py
@@ -1,9 +1,16 @@
 from rest_framework import viewsets
 from .serializer import AcabadoSerializer
 from .models import Acabado
+
+from django.http import JsonResponse
+
 # Create your views here.
 
 class AcabadoViewSet(viewsets.ModelViewSet):
     queryset = Acabado.objects.all()
     serializer_class = AcabadoSerializer
-    
+
+def listar_acabados(request):
+    acabados = list(Acabado.objects.raw('SELECT * FROM acabado'))
+    data = [{'id_acabado': acabado.id_acabado, 'nombre': acabado.nombre} for acabado in acabados]
+    return JsonResponse(data, safe=False)
